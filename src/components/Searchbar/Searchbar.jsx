a0560f3d3d6a3d79ba79b0193df52d45013418e7
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-12-17 23:41:40 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,63 +1,54 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import s from 'components/Styles.module.css';
 import { IconContext } from 'react-icons';
 import { FiSearch } from 'react-icons/fi';
 import { toast } from 'react-toastify';
 
-export default class Searchbar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  state = {
-    value: '',
+export default function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = event => {
+    setQuery(event.currentTarget.value);
   };
 
-  handleInputChange = e => {
-    this.setState({ value: e.currentTarget.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault();
     if (this.state.value.trim() === '') {
       toast.warning('Enter data in the search field!');
       return;
     }
-    this.props.onSubmit(this.state.value.trim().toLowerCase());
-    this.reset();
+
+    onSubmit(query.trim().toLowerCase());
+
+    setQuery('');
   };
 
-  reset = () => {
-    this.setState({ value: '' });
-  };
+  return (
+    <header className={s.Searchbar}>
+      <form className={s.SearchForm} onSubmit={this.handleSubmit}>
+        <button type="submit" className={s.SearchForm_button}>
+          <IconContext.Provider value={{ size: '1.5em' }}>
+            <div>
+              <FiSearch />
+            </div>
+          </IconContext.Provider>
 
-  render() {
-    return (
-      <header className={s.Searchbar}>
-        <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={s.SearchForm_button}>
-            <IconContext.Provider value={{ size: '1.5em' }}>
-              <div>
-                <FiSearch />
-              </div>
-            </IconContext.Provider>
+          <span className={s.SearchForm_button_label}>Search</span>
+        </button>
 
-            <span className={s.SearchForm_button_label}>Search</span>
-          </button>
-
-          <input
-            className={s.SearchForm_input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleInputChange}
-            value={this.state.value}
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={s.SearchForm_input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={this.handleInputChange}
+          value={this.state.value}
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
