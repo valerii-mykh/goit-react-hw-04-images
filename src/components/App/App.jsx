db71d5fb21383d071b9fcda3100cd0d1235a062f
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-12-17 23:52:29 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,6 @@ import pixabayApi from 'components/Api/Api';
 import Modal from 'components/Modal/Modal';
 import Spinner from 'components/Loader/Spinner';
 
-
 export default function App() {
   const [query, setQuery] = useState([]);
   const [status, setStatus] = useState('idle');
@@ -27,107 +26,110 @@ export default function App() {
 
     setStatus('pending');
 
-      pixabayApi
-        .fetchQuery(query, page)
-        .then(({ hits }) => {
-          const images = hits.map(({ id, webformatURL, largeImageURL, tags }) => {
-            return { id, webformatURL, largeImageURL, tags };
-          });
-          // console.log(images);
-          if (images.length > 0) {
-            setQuery(state => [...state, ...images]);
+    pixabayApi
+      .fetchQuery(query, page)
+      .then(({ hits }) => {
+        const images = hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+          return { id, webformatURL, largeImageURL, tags };
+        });
+        // console.log(images);
+        if (images.length > 0) {
+          setQuery(state => [...state, ...images]);
           setStatus('resolved');
-          } else {
-            alert(`По запросу ${query} ничего не найдено.`);
-            this.setState({ status: 'idle' });
-          }
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-  }
+        } else {
+          alert(`По запросу ${query} ничего не найдено.`);
+          setStatus('idle');
+        }
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      })
+      .finally(() => {
+        if (page > 1) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth',
+          });
+        }
+      });
+  }, [page, query]);
 
-  handleSubmitInput = newQuery => {
-    if (newQuery !== this.state.name) {
-      this.setState({ name: newQuery, page: 1, status: 'pending' });
+  const handleSubmitInput = newName => {
+    if (newName !== name) {
+      setQuery([]);
+      setPage(1);
+      setName(newName);
     }
   };
 
-  handleClickImg = event => {
+  const handleClickImg = event => {
     const imgForModal = event.target.dataset.src;
     const altForModal = event.target.alt;
-    this.setState({
-      showModal: true,
-      modalImg: imgForModal,
-      modalAlt: altForModal,
-    });
+    setModalImg(imgForModal);
+    setModalAlt(altForModal);
+    setShowModal(true);
   };
 
-  handleClickBtn = () => {
-    this.setState(({ page }) => {
-      return { page: page + 1, status: 'pending' };
-    });
+  const handleClickBtn = () => {
+    setPage(state => state + 1);
+    setStatus('pending');
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  render() {
-    const { query, showModal, modalImg, modalAlt, error, status } = this.state;
+  if (status === 'idle') {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSubmitInput} />
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    );
+  }
 
-    if (status === 'idle') {
-      return (
+  if (status === 'pending') {
+    // console.log('pending', this.state.query);
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSubmitInput} />
+        {query.length > 0 && <ImageGallery query={query} />}
+        <Spinner className={s.Loader} />
+      </div>
+    );
+  }
+
+  if (status === 'rejected') {
+    return <h1>{error.message}</h1>;
+  }
+
+  if (status === 'resolved') {
+    // console.log('resolved', this.state.query);
+    return (
+      <>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={modalImg} alt={modalAlt} />
+          </Modal>
+        )}
         <div>
           <Searchbar onSubmit={this.handleSubmitInput} />
-          <ToastContainer
-            position="top-center"
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
+          <ImageGallery onClickImg={this.handleClickImg} query={this.state.query} />
+          <Button handleClickBtn={this.handleClickBtn} />
         </div>
-      );
-    }
-
-    if (status === 'pending') {
-      // console.log('pending', this.state.query);
-      return (
-        <div>
-          <Searchbar onSubmit={this.handleSubmitInput} />
-          {query.length > 0 && <ImageGallery query={query} />}
-          <Spinner className={s.Loader} />
-        </div>
-      );
-    }
-
-    if (status === 'rejected') {
-      return <h1>{error.message}</h1>;
-    }
-
-    if (status === 'resolved') {
-      // console.log('resolved', this.state.query);
-      return (
-        <>
-          {showModal && (
-            <Modal onClose={this.toggleModal}>
-              <img src={modalImg} alt={modalAlt} />
-            </Modal>
-          )}
-          <div>
-            <Searchbar onSubmit={this.handleSubmitInput} />
-            <ImageGallery onClickImg={this.handleClickImg} query={this.state.query} />
-            <Button handleClickBtn={this.handleClickBtn} />
-          </div>
-        </>
-      );
-    }
+      </>
+    );
   }
 }
